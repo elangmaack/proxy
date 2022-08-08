b7881566e296dc349319dff28167401f22ac3e6f
Enrique Langmaack <e_langmaack@hotmail.com>
2022-08-08 13:35:17 -0700

    second commit

--- simpleServer_v1.py
+++ simpleServer_v1.py
@@ -4,13 +4,22 @@ import urllib.request
 
 PORT = 9097
 
-
 class MyProxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         url=self.path[1:]
-        self.send_response(200)
-        self.end_headers()
-        self.copyfile(urllib.request.urlopen(url), self.wfile)
+
+
+        #self.copyfile(urllib.request.urlopen(url), self.wfile)
+
+        try:
+            self.send_response(200)
+            self.end_headers()
+            self.copyfile(urllib.request.urlopen(url), self.wfile)
+        except Exception as err:
+            exception_type = type(err).__name__
+            print(exception_type)
+            return
+
 
 httpd = socketserver.ThreadingTCPServer(('', PORT), MyProxy)
 print ("Now serving at")
