091520d8fae29f17bf240eb75284406c967a0dfa
Enrique Langmaack <e_langmaack@hotmail.com>
2022-08-09 08:49:57 -0700

    third commit

--- simpleServer_v1.py
+++ simpleServer_v1.py
@@ -7,18 +7,28 @@ PORT = 9097
 class MyProxy(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         url=self.path[1:]
+        file1 = open("banned_urls.txt", "r")
+        readfile = file1.read()
 
 
         #self.copyfile(urllib.request.urlopen(url), self.wfile)
-
-        try:
-            self.send_response(200)
+        if url in readfile:
+            print("matched")
+            file1.close()
+            self.send_response(403)
             self.end_headers()
-            self.copyfile(urllib.request.urlopen(url), self.wfile)
-        except Exception as err:
-            exception_type = type(err).__name__
-            print(exception_type)
-            return
+
+        else:
+            print("unmatched")
+            file1.close()
+            try:
+                self.send_response(200)
+                self.end_headers()
+                self.copyfile(urllib.request.urlopen(url), self.wfile)
+            except Exception as err:
+                exception_type = type(err).__name__
+                print(exception_type)
+                return
 
 
 httpd = socketserver.ThreadingTCPServer(('', PORT), MyProxy)
